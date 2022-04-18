9b10ea6cc128a741bf7740f85702298b4e603586
Vitalii <0x1d46@gmail.com>
2022-04-18 07:58:57 +0200

    create page 404

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page_404">
+    <h1>404</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageNotFound",
+};
+</script>
+
+<style scoped>
+.page_404 {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  font-size: 240px;
+  color: dimgray;
+}
+</style>
