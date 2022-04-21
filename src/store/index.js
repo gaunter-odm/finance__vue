523778f7f7279dd0015f924e1a1e812a7345dcac
Vitalii <0x1d46@gmail.com>
2022-04-21 11:57:34 +0200

    use auth and loader modules and auth plugin

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
 import { createStore } from "vuex";
+import { authPlugin } from "@/store/plugins/auth.plugin";
+import { auth } from "@/store/modules/auth.module";
+import { loader } from "@/store/modules/loader.module";
 
 export default createStore({
   state: {
@@ -11,5 +14,6 @@ export default createStore({
     setTheme: (state, themeName) => (state.theme = themeName),
   },
   actions: {},
-  modules: {},
+  modules: { auth, loader },
+  plugins: [authPlugin],
 });
