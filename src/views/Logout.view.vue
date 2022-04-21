7a4c6cd8070e3583a63ab705f0d0bbe42f288260
Vitalii <0x1d46@gmail.com>
2022-04-21 11:56:20 +0200

    create Logout view

---
+++ src/views/Logout.view.vue
@@ -0,0 +1,26 @@
+<template></template>
+
+<script>
+import { defineComponent } from "vue";
+import { mapMutations } from "vuex";
+
+import axios from "@/plugins/axios";
+
+export default defineComponent({
+  functional: true,
+  name: "Logout",
+  methods: mapMutations(["logout", "enableLoader", "disableLoader"]),
+  mounted() {
+    this.enableLoader();
+    axios
+      .get("auth/logout")
+      .then(() => {
+        this.logout();
+        this.$router.push({ name: "Login" });
+      })
+      .finally(() => this.disableLoader());
+  },
+});
+</script>
+
+<style scoped></style>
