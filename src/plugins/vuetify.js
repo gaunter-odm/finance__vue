e41a248ba240ae638f102820a310d3fa5fdbe9ce
Vitalii <0x1d46@gmail.com>
2022-04-21 13:19:56 +0200

    add themes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,8 +7,38 @@ import { createVuetify } from "vuetify";
 
 export default createVuetify(
   {
+    lang: { current: "ru" },
     theme: {
-      dark: true,
+      dark: {
+        primary: "#2196F3",
+        secondary: "#424242",
+        accent: "#FF4081",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FB8C00",
+      },
+      light: true,
+    },
+    themes: {
+      light: {
+        primary: "#1976D2",
+        secondary: "#424242",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FB8C00",
+      },
+      dark: {
+        primary: "#2196F3",
+        secondary: "#424242",
+        accent: "#FF4081",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FB8C00",
+      },
     },
   }
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
