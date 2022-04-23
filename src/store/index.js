6087e23a53ef16f87f1ea2f28585c09c1ebb67c1
Vitalii <0x1d46@gmail.com>
2022-04-23 08:00:14 +0200

    use theme module

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { createStore } from "vuex";
 import { authPlugin } from "@/store/plugins/auth.plugin";
 import { auth } from "@/store/modules/auth.module";
 import { loader } from "@/store/modules/loader.module";
+import { theme } from "@/store/modules/theme.module";
 
 export default createStore({
   modules: { auth, loader, theme },
