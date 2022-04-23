7e00a68b59daecd28aee2960743a2c454f7d7aa1
Vitalii <0x1d46@gmail.com>
2022-04-23 08:00:05 +0200

    use theme module

--- src/store/index.js
+++ src/store/index.js
@@ -4,16 +4,6 @@ import { auth } from "@/store/modules/auth.module";
 import { loader } from "@/store/modules/loader.module";
 
 export default createStore({
-  state: {
-    theme: "dark",
-  },
-  getters: {},
-  mutations: {
-    toggleTheme: (state) =>
-      (state.theme = state.theme === "dark" ? "light" : "dark"),
-    setTheme: (state, themeName) => (state.theme = themeName),
-  },
-  actions: {},
-  modules: { auth, loader },
+  modules: { auth, loader, theme },
   plugins: [authPlugin],
 });
