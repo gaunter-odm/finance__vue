c0e5c98b5b414784757793577e78adb6bbaeaf9e
Vitalii <0x1d46@gmail.com>
2022-04-23 07:59:40 +0200

    use Vue theme plugin

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import themePlugin from "@/plugins/theme";
 import "./assets/styles/main.scss";
 
 loadFonts();
 
-createApp(App).use(router).use(store).use(vuetify).mount("#app");
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .use(themePlugin)
+  .mount("#app");
